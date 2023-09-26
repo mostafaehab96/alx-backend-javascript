c2434bfec88136b84b111c1ccfdd8e458fa2f07a
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-09-26 06:05:23 +0300

    Add task 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,6 +1,9 @@
 export default function createReportObject(employeesList) {
   const result = {
     allEmployees: {},
+    getNumberOfDepartments(allEmployees) {
+      return Object.values(allEmployees).length;
+    },
   };
   for (const depart in employeesList) {
     if (employeesList) {
