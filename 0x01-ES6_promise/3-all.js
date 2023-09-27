465cf89651113b093c6bdedaa4b67e53e5003925
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-09-27 03:57:53 +0300

    Fix task 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,5 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   uploadPhoto().then((response) => console.log(response.body))
     .then(createUser().then((response) => console.log(response.firstName, response.lastName)))
-    .catch(console.log('Signup system offline'));
+    .catch(() => console.log('Signup system offline'));
 }
