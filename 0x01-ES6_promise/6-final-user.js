50eb49187656f4af0c17c43633fde819f15b4abb
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-09-28 16:11:12 +0300

    Fix 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,15 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    
-    return signUpUser(firstName, lastName)
+  return signUpUser(firstName, lastName)
     .then(() => {
-        uploadPhoto(fileName)
-        .catch((err) => {
-            [{
-                status: 200,
-                value: err.message
-            }]
-        })
-    })
+      uploadPhoto(fileName)
+        .catch((err) => [{
+          status: 200,
+          value: err.message,
+        }]);
+    });
 }
