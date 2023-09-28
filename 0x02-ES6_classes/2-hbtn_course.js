3edb18712d845256ebdce984e27a47e361b1b93d
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-09-28 21:15:19 +0300

    Add task 2-hbtn_course.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,37 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') this._name = newName;
+    else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength === 'number') this._length = newLength;
+    else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    this._students = newStudents;
+  }
+}
