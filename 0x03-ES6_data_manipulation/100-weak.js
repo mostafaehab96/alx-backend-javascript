b5b5ce502d746e43722913c79e14148e6371635b
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-04 17:12:54 +0300

    Fix 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,8 +3,8 @@ const endpoint = { protocol: 'http', name: 'getUsers' };
 weakMap.set(endpoint, 1);
 export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
-    if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+    if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
   } else {
     weakMap.set(endpoint, 1);
   }
