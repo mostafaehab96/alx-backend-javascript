ebcc68e4df6a60b3a7a97678cbc7d361da55de49
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-04 04:07:56 +0300

    Add task 9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const gorceryMap = new Map();
+  gorceryMap.set('Apples', 10);
+  gorceryMap.set('Tomatoes', 10);
+  gorceryMap.set('Pasta', 1);
+  gorceryMap.set('Rice', 1);
+  gorceryMap.set('Banana', 5);
+
+  return gorceryMap;
+}
