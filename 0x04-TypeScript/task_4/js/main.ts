e1eadb4438053eafeeaaa5998f50b56f2271b4af
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-05 01:52:46 +0300

    Add main.ts in task 4

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,28 @@
+import Cpp = Subjects.Cpp;
+import Java = Subjects.Java;
+import React = Subjects.React;
+import Teacher = Subjects.Teacher;
+
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
+export const cTeacher: Teacher = {
+  firstName: 'Mostafa',
+  lastName: 'Ehab',
+  experienceTeachingC: 10,
+};
+
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
