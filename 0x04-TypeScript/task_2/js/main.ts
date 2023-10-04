7b1ed0b1cca39e8be217ef75f1e1f84b2be97c27
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-05 00:07:25 +0300

    Add task_2 all tasks

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,68 @@
+export interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+export interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+export class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+export class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+export function createEmployee(
+  salary: number | string
+): DirectorInterface | TeacherInterface {
+  if (typeof salary === 'number' && salary < 500) return new Teacher();
+  return new Director();
+}
+
+export function isDirector(
+  employee: DirectorInterface | TeacherInterface
+): boolean {
+  if (employee instanceof Director) return true;
+  return false;
+}
+
+export function executeWork(
+  employee: DirectorInterface | TeacherInterface
+): string {
+  if (isDirector(employee)) return (employee as Director).workDirectorTasks();
+
+  return (employee as Teacher).workTeacherTasks();
+}
+
+export type Subjects = 'Math' | 'History';
+
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') return 'Teaching Math';
+  return 'Teaching History';
+}
