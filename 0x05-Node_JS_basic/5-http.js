8c4686c1dfc805ece42ca1b414eafde5a5f6e940
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2024-01-31 02:03:23 +0200

    Add task 5-http.js

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+
+const dbName = process.argv[2];
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/students') {
+    res.write('This is a list of our students\n');
+    fs.readFile(dbName, 'utf-8', (err, fileContent) => {
+      if (err) {
+        throw new Error('Cannot load the database');
+      } else {
+        const students = fileContent.trim().split('\n').slice(1);
+        const studentsObjects = students.map((student) => {
+          const studentsInfo = student.split(',');
+          return {
+            firstName: studentsInfo[0],
+            secondName: studentsInfo[1],
+            age: studentsInfo[2],
+            field: studentsInfo[3],
+          };
+        });
+        const count = students.length;
+        res.write(`Number of students: ${count}\n`);
+        const fieldsList = {};
+        studentsObjects.forEach((student) => {
+          fieldsList[student.field] = (fieldsList[student.field] || []).concat(student.firstName);
+        });
+        Object.keys(fieldsList).forEach((field) => {
+          const names = fieldsList[field];
+          res.write(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`);
+        });
+        res.end('');
+      }
+    });
+  } else {
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+}).listen(1245);
+
+module.exports = app;
