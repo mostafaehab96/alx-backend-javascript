deed83392e3ab0419c4794bdba30cbb705b543ff
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2024-02-01 21:19:41 +0200

    Add project  0x06-unittests_in_js, README and first task

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,17 @@
+const { assert } = require('chai');
+// eslint-disable-next-line import/extensions
+const calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber Test', () => {
+  // eslint-disable-next-line jest/expect-expect,jest/prefer-expect-assertions
+  it('calculateNumber', () => {
+    assert.equal(calculateNumber(1, 1), 2);
+    assert.equal(calculateNumber(1.2, 2), 3);
+    assert.equal(calculateNumber(1.5, 2), 4);
+    assert.equal(calculateNumber(1.6, 2), 4);
+    assert.equal(calculateNumber(1, 2.1), 3);
+    assert.equal(calculateNumber(1.2, 2.3), 3);
+    assert.equal(calculateNumber(1.5, 2.5), 5);
+    assert.equal(calculateNumber(1.6, 2.6), 5);
+  });
+});
