a3ea7fc6238bf9c618f93eb4b6778fdf71a0551d
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2024-02-02 07:45:16 +0200

    Add colon to the output

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -2,7 +2,7 @@ const Utils = require('./utils');
 
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
   const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is ${result}`);
+  console.log(`The total is: ${result}`);
 };
 
 module.exports = sendPaymentRequestToApi;
