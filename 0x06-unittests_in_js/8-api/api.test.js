0728a4e520611a21493031003819d5893fca9bbd
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2024-02-03 00:35:58 +0200

    Configure the index page test

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -3,13 +3,15 @@ const { expect } = require('chai');
 
 describe('index page', () => {
   /* eslint-disable jest/valid-expect, jest/prefer-expect-assertions, no-unused-expressions */
-  it('should return correct results', () => {
-    request('http://localhost:7865', (err, res, body) => {
+  /* eslint-disable jest/no-test-callback */
+  it('should return correct results', (done) => {
+    request('http://localhost:7865/', 'GET', (err, res, body) => {
       if (!err) {
-        expect(err).to.equal(null);
         expect(res.statusCode).to.equal(200);
-        expect(body).to.equal('Welcome to the payment system');
+        expect(res.request.method).to.equal('GET');
         expect(res.request.port).to.equal('7865');
+        expect(body).to.equal('Welcome to the payment system');
+        done();
       }
     });
   });
